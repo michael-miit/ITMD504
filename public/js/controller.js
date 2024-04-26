ed8fd00a619f7b0ef4269846ce69325319f9b4a5
michael-miit <milawd@gmail.com>
2024-04-26 19:54:45 +0300

    add pagination

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,9 +1,15 @@
 var app = angular.module('myApp', []);
 app.controller('formCtrl', function($scope, $http,$location,$window) {
     $scope.searchQuery = '';
+    $scope.currentPage = 1; // Initialize current page
+    $scope.rowsPerPage = 3; // Set the number of rows per page
+    $scope.startIndex = 0; // Initialize starting index for each page
+    $scope.users = []; // Your entire dataset
+    $scope.filteredUsers = $scope.users; // Initialize the filtered list with all users
+
     /***********For Add student detail into database***********/ 
     $scope.user = function(info) {
-        $http.post('http://64.23.217.89:7000/api/'+'student',info).then(successCallback, errorCallback);
+        $http.post('http://localhost:7000/api/'+'student',info).then(successCallback, errorCallback);
 
         function successCallback(response){
             window.location.href="index.html"
@@ -24,23 +30,36 @@ app.controller('formCtrl', function($scope, $http,$location,$window) {
 
      /***********For Showing All Details of Students in the List***********/ 
      $scope.allUsers=function(){
-         $http.get('http://64.23.217.89:7000/api/'+'allstudent').then(successCallback, errorCallback);
+         $http.get('http://localhost:7000/api/'+'allstudent').then(successCallback, errorCallback);
 
          function successCallback(response){
              $scope.users=response.data;
+             $scope.filteredUsers = $scope.users;
              console.log( $scope.users);
          }
          function errorCallback(error){
              //error code
          }
      };
-     $scope.searchFilter = function(user) {
+     /***********./For Showing All Details of Students in the List***********/
+
+     /***********For Search and Pagination functionality ***********/ 
+    $scope.search = function() {
         var query = $scope.searchQuery.toLowerCase();
-        return (
-          (user.fname && user.fname.toLowerCase().includes(query))
-        );
+        $scope.filteredUsers = $scope.users.filter(function(user) {
+          return (
+            (user.fname && user.fname.toLowerCase().includes(query))
+          );
+        });
+        $scope.currentPage = 1; // Reset current page to 1 when performing a new search
+    };
+
+      $scope.totalPages = function() {
+        return Math.ceil($scope.filteredUsers.length / $scope.rowsPerPage);
       };
-     /***********./For Showing All Details of Students in the List***********/ 
+      $scope.getStartIndex = function() {
+        return ($scope.currentPage - 1) * $scope.rowsPerPage + 1;
+      };
 
     /***********For updating students details from database**************/  
      $scope.editUser=function(id){
@@ -49,7 +68,7 @@ app.controller('formCtrl', function($scope, $http,$location,$window) {
      }
      $scope.students=function(info){
         var id=window.localStorage.getItem('id');
-        $http.post('http://64.23.217.89:7000/api/'+'studentUpdate/'+id,info).then(successCallback, errorCallback);
+        $http.post('http://localhost:7000/api/'+'studentUpdate/'+id,info).then(successCallback, errorCallback);
 
         function successCallback(response){
             window.location.href="index.html"
@@ -60,7 +79,7 @@ app.controller('formCtrl', function($scope, $http,$location,$window) {
     }
     $scope.editId = function() {
         var id=window.localStorage.getItem('id');
-        $http.get('http://64.23.217.89:7000/api/'+'studentDetail/'+id).then(successCallback, errorCallback);
+        $http.get('http://localhost:7000/api/'+'studentDetail/'+id).then(successCallback, errorCallback);
 
         function successCallback(response){
             $scope.userinfo=response.data;
@@ -76,7 +95,7 @@ app.controller('formCtrl', function($scope, $http,$location,$window) {
 
     /***********For deleting students details from database**************/
     $scope.deleteUser=function(id){
-       $http.get('http://64.23.217.89:7000/api/'+'deleteStudents/'+id).then(successCallback, errorCallback);
+       $http.get('http://localhost:7000/api/'+'deleteStudents/'+id).then(successCallback, errorCallback);
 
        function successCallback(response){
            $scope.deleteId=response.data;
@@ -88,7 +107,4 @@ app.controller('formCtrl', function($scope, $http,$location,$window) {
        }
    }
     /***********./For deleting students details from database**************/ 
-
-    
-
 });
